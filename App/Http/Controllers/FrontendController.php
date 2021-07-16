17a611ed157c0a10d77af8dfe9f488f7dfe1cb13
Zun Pwint Phyu <scm.zunpwintphyu@gmail.com>
2021-07-16 14:35:00 +0630

    remove unnecessary

--- App/Http/Controllers/FrontendController.php
+++ App/Http/Controllers/FrontendController.php
@@ -4,12 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Contracts\Services\Post\PostServiceInterface;
-use Auth;
-use Hash;
-use App\Post;
-use App\Models\User;
-use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\File;
 
 
 class FrontendController extends Controller
